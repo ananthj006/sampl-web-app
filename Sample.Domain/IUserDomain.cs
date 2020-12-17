284ac41db262ecefc4725b557e5f2ae377f6a421
ananthj006 <ananthj006@gmail.com>
2020-12-18 00:46:37 +0530

    Add files via upload

---
+++ Sample.Domain/IUserDomain.cs
@@ -0,0 +1,13 @@
+﻿using Sample.DTO;
+using System.Collections.Generic;
+
+namespace Sample.Domain
+{
+    public interface IUserDomain
+    {
+        IEnumerable<User> GetUsers(string search, string sort);
+        int AddUser(User user);
+        int UpdateUser(int userId, User user);
+        int DeleteUser(int userId);
+    }
+}
