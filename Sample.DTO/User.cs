5f6fcf2676a2fdd5b1448ff0fff7bb43fccd03a3
ananthj006 <ananthj006@gmail.com>
2020-12-18 00:45:27 +0530

    Add files via upload

---
+++ Sample.DTO/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sample.DTO
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+    }
+}
